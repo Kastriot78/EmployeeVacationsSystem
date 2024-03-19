626a2b508017d6b73879a1f96cc96bca4cf49f88
Kastriot <67861812+Kastriot78@users.noreply.github.com>
2024-03-19 17:36:16 +0100

    Update vacationRoutes.js

--- backend/Routes/vacationRoutes.js
+++ backend/Routes/vacationRoutes.js
@@ -20,8 +20,7 @@ import {
 
 // router.get('/all', isAuth, isAdmin, getAllVacations);
 router.get('/status/:status', isAuth, isAdmin, getUserVacationsHistoryByStatus);
-router.get('/all/approved', isAuth, getApprovedVacationsWithUserInfo); // admin can view all pending vacations with user
-// router.get('/', isAuth, isAdmin, getPendingVacationsWithUserInfo); // admin can view all pending vacations with user
+router.get('/all/approved', isAuth, getApprovedVacationsWithUserInfo);
 router.get('/:userId', isAuth, getUserVacationHistory);
 router.get('/vacations/today', isAuth, isAdmin, getVacationsForToday);
 router.get('/vacations/week', isAuth, isAdmin, getVacationsForWeek);
@@ -32,5 +31,3 @@ router.put('/update', isAuth, isAdmin, updateVacationStatus);
 router.delete('/remove/:vacationId', isAuth, deleteVacation);
 
 export default router;
-
-// send email to me, when vacation request is created.
