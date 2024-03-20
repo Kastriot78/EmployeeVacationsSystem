e8077ba0664236426ce356bb690609b7032b1c18
Kastriot <67861812+Kastriot78@users.noreply.github.com>
2024-03-20 12:25:55 +0100

    Update apiCalls.js

--- frontend/src/redux/apiCalls.js
+++ frontend/src/redux/apiCalls.js
@@ -51,10 +51,12 @@ export const register = async (user, dispatch) => {
     }
 }
 
-export const login = async (formData, dispatch) => {
+export const login = async (formData, user, dispatch) => {
     dispatch(loginStart());
     try {
-        const res = await api.post(`${apiUrl}/api/users/login`, formData);
+        const res = await axios.post(`${apiUrl}/api/users/login`, formData, {
+            headers: { Authorization: 'Bearer ' + user?.token }
+        });
         dispatch(loginSuccess(res.data));
     } catch (error) {
         dispatch(loginFailure(error?.response?.data?.error));
