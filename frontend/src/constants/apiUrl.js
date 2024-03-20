96470f5440340cad7ed90cd58359f183d8b2e619
Kastriot <67861812+Kastriot78@users.noreply.github.com>
2024-03-20 10:49:24 +0100

    Update apiUrl.js

--- frontend/src/constants/apiUrl.js
+++ frontend/src/constants/apiUrl.js
@@ -1,3 +1,4 @@
-export const apiUrl = 'http://localhost:5000'
+// export const apiUrl = 'http://localhost:5000'
+export const apiUrl = 'https://employee-vacations-api.vercel.app'
 export const apiUrlTodayVacations = 'api/vacations/vacations/today'
 export const apiUrlWeekVacations = 'api/vacations/vacations/week'
